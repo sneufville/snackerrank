3d69a1f092f7c5767812e8bbd5fce26ba4cc4b39
sai <sneufville@academic.rrc.ca>
2024-03-18 22:57:28 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*******w********
+ *
+ * Name: Simon Neufville
+ * Date: March 18, 2024,
+ * Description: Index / homepage of SnackerRank
+ ****************/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>SnackerRank</title>
+</head>
+<body>
+<h1>Welcome to SnackerRank</h1>
+<small>The ultimate snack ranking website ever made</small>
+</body>
+</html>
